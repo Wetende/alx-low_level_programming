5ad790f86c03fc05f77207dc7160ce74a6b12754
“Wetende” <cyprianwetende@gmail.com>
2023-02-17 14:45:22 -0800

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,21 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
- /**
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+  char low;
 
- int main(void)
- {
-   char low;
-
-   for (low = 'a'; low <= 'z'; low++) {
+  for (low = 'a'; low <= 'z'; low++) {
     putchar(low);
   }
 
-   printf("\n");
+  printf("\n");
 
-   return (0);
- }
+  return (0);
+}
--- 0x01-variables_if_else_while/2-print_alphabet.c~
+++ 0x01-variables_if_else_while/2-print_alphabet.c~
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
 
-int main(void)
-{
-  char low;
+ /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-  for (low = 'a'; low <= 'z'; low++) {
+ int main(void)
+ {
+   char low;
+
+   for (low = 'a'; low <= 'z'; low++) {
     putchar(low);
   }
 
-  printf("\n");
+   printf("\n");
 
-  return (0);
-}
+   return (0);
+ }
