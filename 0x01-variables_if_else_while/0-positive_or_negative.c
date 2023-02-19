af3fd38b52da68b0b722fc364daad64a0077cffa
“Wetende” <cyprianwetende@gmail.com>
2023-02-19 12:09:59 -0800

    betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+	* main - Entry point
+	* Description: 'Basic Uses of If else statements'
+	* Return: Always 0 (Success)
+*/
 int main(void)
 {
+	/* The random Integer */
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
+	/* My own Code */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-
 	return (0);
 }
