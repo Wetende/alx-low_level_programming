ecbf9c4acc190c86643acb6820edeca3d5c40d2e
“Wetende” <cyprianwetende@gmail.com>
2023-02-21 06:00:50 -0800

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
