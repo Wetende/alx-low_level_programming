f156c48188706b823971f29bfb92bf50b4eb6d77
“Wetende” <cyprianwetende@gmail.com>
2023-02-23 12:41:19 -0800

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Tests the positive_or_negative function with input 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i = 0;
+
+    printf("Testing positive_or_negative() function with input 0:\n");
+    positive_or_negative(i);
+
+    return (0);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+debuggin in c
+    What is debugging
+    What are some methods of debugging manually
+    How to read the error messages
+
