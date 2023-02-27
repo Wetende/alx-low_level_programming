c20104d442ec8c53169f5968979c5baea4ae5275
“Wetende” <cyprianwetende@gmail.com>
2023-02-27 14:55:37 -0800

    hotfix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,13 +11,14 @@
  */
 int main(void)
 {
+    size_t i;
     char password[PASSWORD_LENGTH + 1] = {0}; /* The generated password */
     const char *valid_chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; /* The set of valid characters for the password */
     const size_t num_valid_chars = sizeof(valid_chars) - 1; /* The number of valid characters in the set */
     
     srand(time(NULL)); /* Seed the random number generator with the current time */
 
-    size_t i;    
+      
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         password[i] = valid_chars[rand() % num_valid_chars]; /* Generate a random password by selecting characters from the valid set */
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-
-	if (dest == NULL || src == NULL)
-		return (NULL);
 	int i = 0;
 
 	for (; src[i] != '\0'; i++)
 	{
-		if (i >= sizeof(dest) - 1)
-		{
-			/* Error: dest buffer too small */
-		return (NULL);
-		}
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
