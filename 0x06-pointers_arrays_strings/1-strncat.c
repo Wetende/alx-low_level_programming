4dee61a460bb3d0418f800f61775a45d5a7534f0
“Wetende” <cyprianwetende@gmail.com>
2023-03-01 02:49:58 -0800

    pointers_arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: input of max bytes to be used
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, co;
+
+	for (x = 0; dest[x] != '\0'; x++)
+	{
+	}
+
+	for (co = 0; co < n; co++)
+	{
+		dest[x + co] = src[co];
+		if (src[co] == '\0')
+			co = n;
+	}
+
+	return (dest);
+}
