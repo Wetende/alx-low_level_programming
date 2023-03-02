6ce6edffdaa7e1808ae10fd0770fec394e330128
“Wetende” <cyprianwetende@gmail.com>
2023-03-02 12:59:37 -0800

    m

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar.
+ *
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	int i = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / i > 9)
+	i *= 10;
+
+	while (i > 0) {
+	_putchar('0' + n / i);
+	n %= i;
+	i /= 10;
+	}
+}
+
