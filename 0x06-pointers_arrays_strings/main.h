db049a97db91cd45fadc493ad56a0bb5386bfc6e
“Wetende” <cyprianwetende@gmail.com>
2023-03-02 12:46:00 -0800

    hotfix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
-
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
