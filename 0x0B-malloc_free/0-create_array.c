9de8f559a9afe42493431400a77959f8c2088d8a
“Wetende” <cyprianwetende@gmail.com>
2023-03-14 07:05:32 -0700

    hotfix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-	arr = (size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 	if (arr == 0)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (unsigned int i = 0; i < size; i++)
 		arr[i] = c;
 
 
